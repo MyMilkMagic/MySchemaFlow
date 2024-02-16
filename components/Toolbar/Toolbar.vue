716f34077b69ac8e607ffeee479a6168ebf88447
MyMilkMagic <kapitan.heneral.dev@outlook.com>
2024-02-16 16:12:09 +0800

    Add export image button

--- components/Toolbar/Toolbar.vue
+++ components/Toolbar/Toolbar.vue
@@ -1,10 +1,14 @@
 <script lang="ts" setup>
 import OpenFile from '@components/Toolbar/Partials/OpenFile.vue';
+import ExportImage from '@components/Toolbar/Partials/ExportImage.vue';
 </script>
 <template>
   <nav
     class="flex items-center justify-between border-b-2 border-b-slate-300 bg-white px-4 py-2"
   >
-    <OpenFile />
+    <div class="flex">
+      <OpenFile />
+      <ExportImage />
+    </div>
   </nav>
 </template>
