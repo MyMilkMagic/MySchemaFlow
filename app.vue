d43b20e7b35829484ec038fed9f5f9a23716e962
MyMilkMagic <kapitan.heneral.dev@outlook.com>
2024-02-17 07:24:10 +0800

    Make canvas adapt different screen heights

--- app.vue
+++ app.vue
@@ -4,12 +4,30 @@ import Toolbar from '@components/Modules/Toolbar/Toolbar.vue';
 import { vueFlowKey } from '~/symbols/VueFlowSymbol';
 import { useVueFlow } from '@vue-flow/core';
 
+const toolbar = ref<InstanceType<typeof Toolbar>>();
+const canvas = ref<InstanceType<typeof CanvasBoard>>();
+const calculateHeight = () => {
+  if (!toolbar.value || !canvas.value) return;
+  const { height } = toolbar.value.$el.getBoundingClientRect();
+  const Height = window.innerHeight - height;
+  Object.assign(canvas.value.$el.style, {
+    height: `${Height}px`,
+  });
+};
 provide(vueFlowKey, useVueFlow());
+onMounted(async () => {
+  await nextTick();
+  calculateHeight();
+  window.addEventListener('resize', calculateHeight);
+});
+onUnmounted(() => {
+  window.removeEventListener('resize', calculateHeight);
+});
 </script>
 
 <template>
   <div class="font-dm">
-    <Toolbar />
-    <CanvasBoard />
+    <Toolbar ref="toolbar" />
+    <CanvasBoard ref="canvas" />
   </div>
 </template>
