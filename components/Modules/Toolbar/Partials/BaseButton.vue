b7ab9056c6f56c44e28d1decd24841f8915344ff
MyMilkMagic <kapitan.heneral.dev@outlook.com>
2024-02-17 19:09:43 +0800

    Add tooltip to import button

--- components/Modules/Toolbar/Partials/BaseButton.vue
+++ components/Modules/Toolbar/Partials/BaseButton.vue
@@ -1,8 +1,17 @@
+<script lang="ts" setup>
+import BaseTooltipWithShortcutKeys from '@components/Base/BaseTooltipWithShortcutKeys.vue';
+</script>
 <template>
-  <button
-    type="button"
-    class="flex h-[30px] w-[30px] items-center justify-center rounded-md bg-white stroke-slate-900 p-1.5 outline-none hover:bg-blue-100 hover:stroke-blue-500"
-  >
-    <slot></slot>
-  </button>
+  <BaseTooltipWithShortcutKeys>
+    <button
+      type="button"
+      class="flex h-[30px] w-[30px] items-center justify-center rounded-md bg-white stroke-slate-900 p-1.5 outline-none hover:bg-blue-100 hover:stroke-blue-500"
+    >
+      <slot></slot>
+    </button>
+
+    <template #tooltip>
+      <slot name="tooltip"></slot>
+    </template>
+  </BaseTooltipWithShortcutKeys>
 </template>
