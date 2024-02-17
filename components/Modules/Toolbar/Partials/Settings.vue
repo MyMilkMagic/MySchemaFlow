9259189a4ea663fdf3b04fdf2e332a7f562a3596
MyMilkMagic <kapitan.heneral.dev@outlook.com>
2024-02-17 19:12:35 +0800

    Add tooltip to import, export, and settings buttons

--- components/Modules/Toolbar/Partials/Settings.vue
+++ components/Modules/Toolbar/Partials/Settings.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import SharedSettingsIcon from '@components/Shared/Icons/SharedSettingsIcon.vue';
 import BaseButton from '@components/Modules/Toolbar/Partials/BaseButton.vue';
+import BaseTooltipLabel from '@components/Base/BaseTooltipLabel.vue';
 </script>
 
 <template>
   <BaseButton>
     <SharedSettingsIcon />
+
+    <template #tooltip>
+      <BaseTooltipLabel>Settings</BaseTooltipLabel>
+    </template>
   </BaseButton>
 </template>
