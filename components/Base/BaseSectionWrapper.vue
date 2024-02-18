079a452c023c4aa6a45ce7b09b8aac601f46c464
MyMilkMagic <kapitan.heneral.dev@outlook.com>
2024-02-18 13:02:27 +0800

    Add focus visible state

--- components/Base/BaseSectionWrapper.vue
+++ components/Base/BaseSectionWrapper.vue
@@ -29,21 +29,22 @@ const onClickToggleWrapper = () => {
 <template>
   <div>
     <button
-      class="group flex w-full items-center justify-between border-b-2 bg-slate-200 px-2 py-2 outline-none hover:bg-blue-100 focus:bg-blue-100"
+      class="group flex w-full items-center justify-between border-b-2 bg-slate-200 px-2 py-2 outline-none hover:bg-blue-100 focus-visible:bg-blue-100"
       :class="{
-        'border-b-slate-400/50': !displayContent,
+        'border-b-slate-400/50 hover:border-b-blue-300 focus-visible:border-b-blue-300':
+          !displayContent,
         'border-b-transparent': displayContent,
       }"
       type="button"
       @click="onClickToggleWrapper"
     >
       <span
-        class="text-xs font-bold text-slate-600 group-hover:text-blue-600 group-focus:text-blue-600"
+        class="text-xs font-bold text-slate-600 group-hover:text-blue-600 group-focus-visible:text-blue-600"
         ><slot name="label"></slot
       ></span>
       <span
         ref="chevronIcon"
-        class="w-[10px] stroke-slate-600 group-hover:stroke-blue-600 group-focus:stroke-blue-600"
+        class="w-[10px] stroke-slate-600 group-hover:stroke-blue-600 group-focus-visible:stroke-blue-600"
       >
         <SharedChevronIcon />
       </span>
