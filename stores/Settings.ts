02b5c4006cc4553021d813d5d0b0becf161ac469
MyMilkMagic <kapitan.heneral.dev@outlook.com>
2024-02-18 15:24:36 +0800

    Add functionality to navigate tables from canvas

--- stores/Settings.ts
+++ stores/Settings.ts
@@ -3,5 +3,7 @@ import { defineStore } from 'pinia';
 export const useSettingsStore = defineStore('settings', {
   state: () => ({
     currentOrientation: 'TB',
+    zoomLevel: 1,
+    enableTableJumpAnimation: true,
   }),
 });
