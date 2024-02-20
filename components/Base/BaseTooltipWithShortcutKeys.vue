69ef23e5c39e52ec1b2c7fa07edf97892ac7c8c2
MyMilkMagic <kapitan.heneral.dev@outlook.com>
2024-02-20 15:26:24 +0800

    Fix tooltip floating bug

--- components/Base/BaseTooltipWithShortcutKeys.vue
+++ components/Base/BaseTooltipWithShortcutKeys.vue
@@ -5,8 +5,11 @@ const referenceEl = ref<HTMLButtonElement>();
 const floatingEl = ref<HTMLDivElement>();
 const floatingArrow = ref<HTMLDivElement>();
 const showTooltip = ref(false);
-const { arrowStyles, floatingStyles, onBeforeEnter, onEnter, onLeave } =
-  useTooltip(referenceEl, floatingEl, floatingArrow, showTooltip);
+const { onBeforeEnter, onEnter, onLeave } = useTooltip(
+  referenceEl,
+  floatingEl,
+  floatingArrow,
+);
 
 defineExpose({
   showTooltip,
@@ -31,11 +34,9 @@ defineExpose({
           v-if="showTooltip"
           ref="floatingEl"
           class="absolute z-50 block rounded border-[1px] border-blue-500 bg-white px-3 py-2 text-xs shadow-[inset_0_-3px_0_0_#3b82f687]"
-          :style="floatingStyles"
         >
           <span
             ref="floatingArrow"
-            :style="arrowStyles"
             class="absolute block -translate-y-full border-[6px] border-transparent border-b-blue-500"
           />
           <slot name="tooltip"></slot>
--- composables/Miscellaneous/useTooltip.ts
+++ composables/Miscellaneous/useTooltip.ts
@@ -1,5 +1,5 @@
 import anime from 'animejs/lib/anime.es.js';
-import { arrow, flip, shift, useFloating, autoUpdate } from '@floating-ui/vue';
+import { arrow, flip, shift, computePosition } from '@floating-ui/dom';
 import type { MaybeElement } from '@floating-ui/vue';
 import type { Ref } from 'vue';
 
@@ -7,73 +7,76 @@ export function useTooltip(
   sourceEl: Readonly<Ref<MaybeElement<HTMLElement>>>,
   floatingEl: Readonly<Ref<MaybeElement<HTMLElement>>>,
   floatingArrow: Readonly<Ref<MaybeElement<HTMLElement>>>,
-  isOpen: Ref<boolean>,
 ) {
-  const { floatingStyles, placement, middlewareData, x, y, isPositioned } =
-    useFloating(sourceEl, floatingEl, {
-      middleware: [shift(), flip(), arrow({ element: floatingArrow })],
-      whileElementsMounted: autoUpdate,
-      open: isOpen,
-    });
-  const arrowStyles = computed(() => {
-    return {
-      left:
-        middlewareData.value.arrow?.x != null
-          ? `${middlewareData.value.arrow.x}px`
-          : 0,
-      top:
-        middlewareData.value.arrow?.y != null
-          ? `${middlewareData.value.arrow.y}px`
-          : 0,
-    };
-  });
   const ArrowWidth = 6;
   const Offset = 15;
+  let translateY = 0;
 
   const onBeforeEnter = (el: Element) => {
     if (el instanceof HTMLElement) {
       Object.assign(el.style, {
-        opacity: 0,
-        transform: `translateX(-10000%)`,
+        opacity: 1,
         zIndex: -9999,
       });
     }
   };
   const onEnter = async (el: Element, done: () => void) => {
+    const AreAllComponentsExist =
+      !sourceEl.value || !floatingEl.value || !floatingArrow.value;
+    if (AreAllComponentsExist) return;
+
     await nextTick();
 
-    const checkDOMExistence = () => {
-      setTimeout(() => {
-        if (!isPositioned.value) checkDOMExistence();
+    const IsHTMLInstance =
+      sourceEl.value instanceof HTMLElement &&
+      floatingEl.value instanceof HTMLElement &&
+      floatingArrow.value instanceof HTMLElement;
 
-        const TranslateY = y.value + ArrowWidth;
+    if (IsHTMLInstance) {
+      const Middlewares = [
+        shift(),
+        flip(),
+        arrow({ element: floatingArrow.value }),
+      ];
+      const { x, y, middlewareData } = await computePosition(
+        sourceEl.value,
+        floatingEl.value,
+        {
+          middleware: Middlewares,
+        },
+      );
 
-        // Bottom part only
-        if (el instanceof HTMLElement) {
-          Object.assign(el.style, {
-            zIndex: null,
-            opacity: 1,
-            transform: `translateX(${x.value}px) translateY(${TranslateY + Offset}px)`,
-          });
+      translateY = y + ArrowWidth;
 
-          anime({
-            targets: el,
-            translateY: TranslateY,
-            opacity: 1,
-            easing: 'easeOutExpo',
-            duration: 350,
-            complete: done,
-          });
-        }
-      }, 150);
-    };
-    checkDOMExistence();
+      Object.assign(floatingArrow.value.style, {
+        left:
+          middlewareData.arrow?.x != null ? `${middlewareData.arrow.x}px` : 0,
+        top:
+          middlewareData.arrow?.y != null ? `${middlewareData.arrow.y}px` : 0,
+      });
+
+      Object.assign(floatingEl.value.style, {
+        zIndex: null,
+        opacity: 1,
+        top: 0,
+        left: 0,
+        transform: `translateX(${x}px) translateY(${translateY + Offset}px)`,
+      });
+
+      anime({
+        targets: el,
+        translateY: translateY,
+        opacity: 1,
+        easing: 'easeOutExpo',
+        duration: 350,
+        complete: done,
+      });
+    }
   };
   const onLeave = (el: Element, done: () => void) => {
-    const TranslateY = y.value + ArrowWidth;
     anime({
       targets: el,
-      translateY: TranslateY + Offset,
+      translateY: translateY + Offset,
       opacity: 0,
       easing: 'easeOutExpo',
       duration: 150,
@@ -82,10 +85,6 @@ export function useTooltip(
   };
 
   return {
-    floatingStyles,
-    arrowStyles,
-    placement,
-    middlewareData,
     onBeforeEnter,
     onEnter,
     onLeave,
